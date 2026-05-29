45abe01ae7c29032312477ea30367d20220c5c9f
Miloslav Šmíd <97102231+M1l069@users.noreply.github.com>
2026-05-30 01:44:12 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Admin',
             'username' => 'admin',
             'email' => 'admin@skola.sk',
